58e5580ca1dc4718f867b00bc9b586c767ced46d
samirkhanal35 <samirkhanal35@gmail.com>
2020-03-09 14:57:03 +0545

    face and eye detection

--- main.py
+++ main.py
@@ -1,38 +1,56 @@
-#https://github.com/opencv/opencv/tree/master/data/haarcascades
-#https://github.com/Mjrovai/OpenCV-Face-Recognition/blob/master/FaceDetection/faceSmileEyeDetection.py
-
+import numpy as np
 import cv2
-import sys
 
-cascPath = sys.argv[1]
-faceCascade = cv2.CascadeClassifier(cascPath)
+# multiple cascades: https://github.com/Itseez/opencv/tree/master/data/haarcascades
+faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eyeCascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+smileCascade = cv2.CascadeClassifier('haarcascade_smile.xml')
 
-video_capture = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)  # set Width
+cap.set(4, 480)  # set Height
 
 while True:
-    # Capture frame-by-frame
-    ret, frame = video_capture.read()
-
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
+    ret, img = cap.read()
+    img = cv2.flip(img, -1)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = faceCascade.detectMultiScale(
         gray,
-        scaleFactor=1.1,
+        scaleFactor=1.3,
         minNeighbors=5,
-        minSize=(30, 30),
-        flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+        minSize=(30, 30)
     )
 
-    # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = img[y:y + h, x:x + w]
 
-    # Display the resulting frame
-    cv2.imshow('Video', frame)
+        eyes = eyeCascade.detectMultiScale(
+            roi_gray,
+            scaleFactor=1.5,
+            minNeighbors=5,
+            minSize=(5, 5),
+        )
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+
+        smile = smileCascade.detectMultiScale(
+            roi_gray,
+            scaleFactor=1.5,
+            minNeighbors=15,
+            minSize=(25, 25),
+        )
+
+        for (xx, yy, ww, hh) in smile:
+            cv2.rectangle(roi_color, (xx, yy), (xx + ww, yy + hh), (0, 255, 0), 2)
+
+        cv2.imshow('video', img)
+
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:  # press 'ESC' to quit
         break
 
-# When everything is done, release the capture
-video_capture.release()
+cap.release()
 cv2.destroyAllWindows()
